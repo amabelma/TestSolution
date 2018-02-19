450fce255a6c74863034bcbf771d2110f2a862dc
Amabeli <mark.amabeli@paragon-inc.com>
2018-02-19 11:02:55 -0500

    Initial Commit. Includes a sln file, a Common and API console project, and mstest UnitTest projects for both Common and API.

---
+++ TestSolution.API.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolution.API.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TestSolution.API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestSolution.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
